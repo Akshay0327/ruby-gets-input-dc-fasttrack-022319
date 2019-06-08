836c01d226294359d4dbd36e4cc3ff075649c0d8
Akshay Sharma <akshay.sharma09@gmail.com>
2019-06-08 18:33:46 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,5 +1,5 @@
-def greeting
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+def greeting(name)
+  puts "Hi, " name "! Welcome to the wonderful world of Ruby programming."
 end
 
 puts "Hi! Welcome to the wonderful world of Ruby programming."
